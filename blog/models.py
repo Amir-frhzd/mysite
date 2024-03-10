adef73b64ce034ae6a7650a2b1a1cc084f256698
xzx <goodstar40@yahoo.com>
2024-03-10 19:16:27 +0330

    created blog model named Post and Contact model

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,20 @@ from django.db import models
 
 
 class Post(models.Model):
+    # iamge =
+    # author =
     title = models.CharField(max_length=255)
     content = models.TextField()
+    # tags =
+    # category =
+    counted_view = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return ("%s-%s"%(self.title,self.id))
+    class Meta:
+        ordering = ['-created_date']
+
 # Create your models here.
