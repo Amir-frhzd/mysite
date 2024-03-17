398e1444bba453345bbd870bc36b6c3135475f00
xzx <goodstar40@yahoo.com>
2024-03-17 12:59:07 +0330

    Increment the view count for each post Use the post ID to open and navigate to the blog post URL Save the updated post information after incrementing the view count

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from blog.models import Post
+from django.utils import timezone
 
 def blog_view(request):
+    
+    posts=Post.objects.filter(published_date__lte=timezone.now())
+    
+    context ={'posts':posts}
+    
 
-    return render(request,"blog/blog-home.html")
+    return render(request,"blog/blog-home.html",context)
 
-def blog_single(request):
+def blog_single(request,pid):
+    posts = get_object_or_404(Post,pk=pid)
+    posts.counted_view += 1
+    posts.save()
+    context ={'posts':posts}
 
-    return render(request,'blog/blog-single.html')
+ 
+    return render(request,'blog/blog-single.html',context)
 
-def test(request):
-    posts = Post.objects.filter(status=1)
+def test(request,pid):
+    posts = get_object_or_404(Post,pk=pid)
     context ={'posts':posts}
     return render(request,'test.html',context)
 
+
+    #your code
 # Create your views here.
