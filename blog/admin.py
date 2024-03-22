109328df37e5999ef3738aa03d9aa29b2c63197a
xzx <goodstar40@yahoo.com>
2024-03-22 14:54:01 +0430

    The section of blog views has become more complete

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post,Category
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
 
     date_hierarchy = "created_date"
     empty_value_display = "-empty-"
-    list_display = ["title","counted_view","status","published_date"]
+    list_display = ["title","author","counted_view","status","published_date"]
     #ordering = ["-created_date"]
     search_fields = ["title","content"]
     list_filter = ('status',)
 
+admin.site.register(Category)
 admin.site.register(Post,PostAdmin)
--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 def blog_view(request):
     
-    posts=Post.objects.filter(published_date__lte=timezone.now())
+    posts=Post.objects.filter(published_date__lte=timezone.now(),status=1)
     
     context ={'posts':posts}
     
@@ -12,10 +12,13 @@ def blog_view(request):
     return render(request,"blog/blog-home.html",context)
 
 def blog_single(request,pid):
-    posts = get_object_or_404(Post,pk=pid)
+    posts = get_object_or_404(Post,pk=pid,status=1,published_date__lte=timezone.now())
+    all_post=Post.objects.filter(status=1,published_date__lte=timezone.now())
+    next_post =all_post.filter(id__gt=pid).order_by('id').first()
+    previous_post=all_post.filter(id__lt=pid).order_by('-id').first()
     posts.counted_view += 1
     posts.save()
-    context ={'posts':posts}
+    context ={'posts':posts,'next_post':next_post,'previous_post':previous_post}
 
  
     return render(request,'blog/blog-single.html',context)
