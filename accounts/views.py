c7ee9fd29167fca3aa1896c651b6eccb5718e11d
xzx <goodstar40@yahoo.com>
2024-05-30 16:56:30 +0430

    accounts:urls.py complete fo authenticate and password_reset_done added

--- accounts/views.py
+++ accounts/views.py
@@ -82,4 +82,4 @@ class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
                       "if an account exists with the email you entered. You should receive them shortly." \
                       " If you don't receive an email, " \
                       "please make sure you've entered the address you registered with, and check your spam folder."
-    success_url = reverse_lazy('website:index')
+    success_url = reverse_lazy('accounts:password_reset_done')
